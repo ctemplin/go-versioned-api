ee2c0eacb264ff061bb6752710c19656465859dc
Chris Templin <ctemplin@gmail.com>
2015-07-14 09:17:10 -0400

    Reordering members.

--- test.go
+++ test.go
@@ -9,6 +9,23 @@ import (
     "dev/handlers/v2"
 )
 
+var acceptVersionMap = map[string]api.API {
+	"application/vnd+json": new(handlersv1.APIv1),
+	"application/vnd.ctemplin.v2+json": new(handlersv2.APIv2),
+}
+
+func ApiVersionMiddleware(w http.ResponseWriter, r *http.Request) {
+	accept := r.Header["Accept"]
+	api, exists := acceptVersionMap[accept[0]]
+	var version string
+	if exists {
+		version = api.Version()
+	} else {
+		version = "unknown"
+	}
+	w.Header().Set("X-ctemplin-version", version)
+}
+
 func Server() *negroni.Negroni {
 	router := mux.NewRouter()
 	router.Path("/")
@@ -32,23 +49,6 @@ func Server() *negroni.Negroni {
 	n.UseHandlerFunc(ApiVersionMiddleware)
 	n.UseHandler(router)
 	return n
-}
-
-var acceptVersionMap = map[string]api.API {
-	"application/vnd+json": new(handlersv1.APIv1),
-	"application/vnd.ctemplin.v2+json": new(handlersv2.APIv2),
-}
-
-func ApiVersionMiddleware(w http.ResponseWriter, r *http.Request) {
-	accept := r.Header["Accept"]
-	api, exists := acceptVersionMap[accept[0]]
-	var version string
-	if exists {
-		version = api.Version()
-	} else {
-		version = "unknown"
-	}
-	w.Header().Set("X-ctemplin-version", version)
 }
 
 func main() {
