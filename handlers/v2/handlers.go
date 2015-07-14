07dbb02bc2df948d524c29bb61ecb9aea7a79fb5
Chris Templin <ctemplin@gmail.com>
2015-07-14 09:43:32 -0400

    Removing commented out func.

--- handlers/v2/handlers.go
+++ handlers/v2/handlers.go
@@ -18,9 +18,3 @@ func (api *APIv2) JsonHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(`{"hi": "hello"}` + "\n"))
 }
 
-// Commented out as redundant. When not defined the "parent" handler
-// is called automatically.
-// func (api *APIv2) JsonHandler2(w http.ResponseWriter, r *http.Request) {
-// 	api.APIv1.JsonHandler2(w, r)
-// }
-
