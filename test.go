3f74123f2338a98d41abcc642e7907b435e5fdb5
Chris Templin <ctemplin@gmail.com>
2015-07-13 21:15:04 -0400

    commenting.

--- test.go
+++ test.go
@@ -19,12 +19,15 @@ func Server() *negroni.Negroni {
 	}
 
 	for acceptHeader, vApi := range acceptVersionMap {
+		// Create a subrouter for the header/api version.
 		subrouter := router.Headers("Accept", acceptHeader).Subrouter()
 
+		// Define the path/handler relationships.
 		pathHandlerMap := map[string]func(http.ResponseWriter, *http.Request) {
 			"/json.json": vApi.JsonHandler,
 			"/json2.json": vApi.JsonHandler2,
 		}
+		// Create a route in the subrouter for each path/handler.
 		for path, handler := range pathHandlerMap {
 			subrouter.HandleFunc(path, handler)
 		}
