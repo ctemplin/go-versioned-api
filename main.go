0df0fd555d123c94204be774be938d14dbfffeea
Chris Templin <ctemplin@gmail.com>
2015-07-17 17:26:40 -0400

    Replace Header matcher with a custom matcher function that checks for Accept header OR a query param names "apiv". This allows for easy api viewing in the browser without needing something like curl to set headers.

--- main.go
+++ main.go
@@ -8,12 +8,19 @@ import (
 	"dev/handlers/v1"
 	"dev/handlers/v2"
 	"dev/handlers/v3"
+	// "fmt"
 )
 
 var acceptVersionMap = map[string]api.API {
 	"application/vnd+json": new(handlersv1.APIv1),
 	"application/vnd.example.v2+json": new(handlersv2.APIv2),
 	"application/vnd.example.v3+json": new(handlersv3.APIv3),
+}
+
+var queryVersionMap = map[string]api.API {
+	"1": new(handlersv1.APIv1),
+	"2": new(handlersv2.APIv2),
+	"3": new(handlersv3.APIv3),
 }
 
 func ApiVersionMiddleware(w http.ResponseWriter, r *http.Request) {
@@ -23,7 +30,7 @@ func ApiVersionMiddleware(w http.ResponseWriter, r *http.Request) {
 	if exists {
 		version = api.Version()
 	} else {
-		version = "unknown"
+		version = r.FormValue("apiv")
 	}
 	w.Header().Set("X-example-version", version)
 }
@@ -32,13 +39,23 @@ func ContentTypeMiddleware(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 }
 
+func acceptOrQueryMatcherFactory(acceptHeader string) (func(*http.Request, *mux.RouteMatch) bool) {
+	return func(r *http.Request, rm *mux.RouteMatch) bool {
+		var isHeaderMatch bool = r.Header["Accept"][0] == acceptHeader
+		var isQueryStringMatch bool = r.FormValue("apiv") == acceptVersionMap[acceptHeader].Version()
+		return isHeaderMatch || isQueryStringMatch
+	}
+}
+
 func Server() *negroni.Negroni {
 	router := mux.NewRouter()
 	router.Path("/")
 
+
 	for acceptHeader, vApi := range acceptVersionMap {
 		// Create a subrouter for the header/api version.
-		subrouter := router.Headers("Accept", acceptHeader).Subrouter()
+		subrouter := router.MatcherFunc(
+			acceptOrQueryMatcherFactory(acceptHeader)).Subrouter()
 
 		// Define the path/handler relationships.
 		pathHandlerMap := map[string]func(http.ResponseWriter, *http.Request) {
--- main_test.go
+++ main_test.go
@@ -65,6 +65,54 @@ func TestAPIVersionByAcceptHeader(t *testing.T) {
 	}
 }
 
+// TestAPIVersionByAcceptHeader checks that values in the request's
+// accept header are mapping to the proper version of the API.
+func TestAPIVersionByQueryString(t *testing.T) {
+
+	n := Server()
+
+	// For each version make a request and check the version in
+	// the response
+	for versionString, acceptHeader := range versions {
+
+		headers := map[string][]string{
+			"Accept": {"*/*"},
+		} 
+
+		for _, testPath := range testPaths {
+
+			response := httptest.NewRecorder()
+
+			url1 := url.URL{Host: "localhost", Path: testPath, RawQuery: "apiv="+versionString}
+
+			request := http.Request{
+				URL: &url1,
+				Header: headers,
+			}
+
+			respObj := struct {
+				Hi string
+			}{}
+
+			n.ServeHTTP(response, &request)
+			// fmt.Print(response.Body)
+			err := json.Unmarshal(response.Body.Bytes(), &respObj)
+			if err != nil {
+				t.Error(err)
+			}
+
+			responseVersion, exists := response.Header()["X-Example-Version"]
+			if !exists {
+				t.Error("API version missing from response headers.")
+			}
+			if versionString != responseVersion[0]  {
+				t.Errorf("Wrong API version returned using Accept-Header: %s. Expected: %d, Got: %d", acceptHeader, versionString, responseVersion[0])
+			}
+		}
+	}
+}
+
+
 // TestNotAcceptable checks that the 406 response is returned when proper.
 func TestNotAcceptable(t *testing.T) {
 
